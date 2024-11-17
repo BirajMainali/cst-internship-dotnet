c369de838e0810529d3113cdd66e5f1d719e8867
Biraj Mainali <birajmainali10@gmail.com>
2024-11-17 17:21:09 +0545

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cst_internship_dotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
